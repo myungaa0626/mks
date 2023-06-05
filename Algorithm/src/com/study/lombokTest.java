b1f77f7baa062f1dd5f3237653d76d4c2effe078
mkshim <myungaa0626@gmail.com>
2023-06-05 18:51:30 +0900

    git push 2023-06-05

---
+++ Algorithm/src/com/study/lombokTest.java
@@ -0,0 +1,12 @@
+package com.study;
+
+import lombok.Data;
+
+@Data
+public class lombokTest {
+
+    private Long a;
+    private String b;
+    private String c;
+    private boolean d;
+}
---
+++ Algorithm/src/com/study/test.java
@@ -0,0 +1,11 @@
+package com.study;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		lombokTest tt = new lombokTest();
+		
+	}
+
+}
