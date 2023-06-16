1c64e8df9061cb8c2188447b38ad902c4002cecb
mkshim <myungaa0626@gmail.com>
2023-06-16 18:07:55 +0900

    algorithm update 2023.06.16

---
+++ bakjoonProblem/src/bakjoonProblem/bakjoon11659.java
@@ -0,0 +1,36 @@
+package bakjoonProblem;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class bakjoon11659 {
+		
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int start = 0;
+		int end = 0;
+		
+		int[] arr = new int[n];
+	
+		
+		for(int i = 0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i = 0; i<m; i++) {
+			arr[i]= sc.nextInt();
+			start= arr[i];
+		
+
+		}
+		
+		
+		
+		
+		
+	}
+}		
