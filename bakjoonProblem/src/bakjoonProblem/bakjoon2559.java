2733735115e313d7e01872c4d14974f83e098afb
mkshim <myungaa0626@gmail.com>
2023-06-19 21:26:37 +0900

    algorithm update 2023.06.19

--- bakjoonProblem/src/bakjoonProblem/bakjoon2559.java
+++ bakjoonProblem/src/bakjoonProblem/bakjoon2559.java
@@ -1,39 +1,41 @@
 package bakjoonProblem;
 
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class bakjoon2559 {
 		
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-//		int m = sc.nextInt();
-		
-		int[] arr = new int[n];
-		
-		for(int i=0; i<n; i++) {
-			arr[i]=sc.nextInt();
+		int m = sc.nextInt();
+		int sum=0;
+		int maxSum = sum;
+		int[] arr = new int[n+1];
+
+		for(int i=1; i<=n; i++) {
 			
-			for(int j=0; j<2; j++) {
-				
-				
-				
-			}
-			
-			
-	
+			arr[i] = sc.nextInt();
+
 		}
 		
 		
+		for(int i =1; i<=m; i++) {
+			
+			sum+=arr[i];
+		}
+		System.out.println(sum);
+		for(int i=1; i<=n-m; i++) {
+			sum = sum-arr[i]+arr[i+m];
+			if(sum>maxSum) {
+				maxSum =sum;
+			}	
+		}
+		System.out.println(maxSum);
 		
-		
-		
-		
-		
-		
-		
-
+	
 	}		
 }		
---
+++ bakjoonProblem/src/bakjoonProblem/practiceTest.java
@@ -0,0 +1,25 @@
+package bakjoonProblem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class practiceTest {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), ".");
+		
+		while (st.hasMoreTokens()) {
+			
+			System.out.println(st.nextToken());
+			
+		}
+		
+		
+		
+
+	}
+
+}
